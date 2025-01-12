30a1f9995d6be969b24431fa00ba238e6a61f6fd
wh1tak3r <83946675+0xWh1tak3r@users.noreply.github.com>
2025-01-12 05:53:28 +0400

    Updated sdk context in test_pinecone_retrieve.py

--- tests/tools/retriever/test_pinecone_retrieve.py
+++ tests/tools/retriever/test_pinecone_retrieve.py
@@ -2,8 +2,8 @@ import os
 import unittest
 from unittest.mock import ANY, MagicMock, patch
 
-from swarmzero.sdk_context import SDKContext  # Import SDKContext
-from swarmzero.tools.retriever.pinecone_retrieve import PineconeRetriever
+from breachswarm.sdk_context import SDKContext  # Import SDKContext
+from breachswarm.tools.retriever.pinecone_retrieve import PineconeRetriever
 
 
 class TestPineconeRetriever(unittest.TestCase):
@@ -21,18 +21,18 @@ class TestPineconeRetriever(unittest.TestCase):
         self.mock_sdk_context.get_utility.return_value = MagicMock()
         self.retriever.sdk_context = self.mock_sdk_context
 
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.PineconeVectorStore')
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.StorageContext')
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.VectorStoreIndex')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.PineconeVectorStore')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.StorageContext')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.VectorStoreIndex')
     def test_create_index(self, VectorStoreIndexMock, StorageContextMock, PineconeVectorStoreMock):
         self.retriever._load_documents = MagicMock(return_value=(['doc1', 'doc2'], ['file1.txt', 'file2.txt']))
 
         index, file_names = self.retriever.create_serverless_index(file_path='dummy_path')
 
         self.pinecone_client_mock.create_index.assert_called_once_with(
-            name="swarmzero-pinecone", dimension=1536, metric="euclidean", spec=ANY
+            name="breachswarm-pinecone", dimension=1536, metric="euclidean", spec=ANY
         )
-        self.pinecone_client_mock.Index.assert_called_once_with("swarmzero-pinecone")
+        self.pinecone_client_mock.Index.assert_called_once_with("breachswarm-pinecone")
         PineconeVectorStoreMock.assert_called_once()
         StorageContextMock.from_defaults.assert_called_once()
         VectorStoreIndexMock.from_documents.assert_called_once_with(
@@ -43,18 +43,18 @@ class TestPineconeRetriever(unittest.TestCase):
         self.assertEqual(index, VectorStoreIndexMock.from_documents.return_value)
         self.assertEqual(file_names, ['file1.txt', 'file2.txt'])
 
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.PineconeVectorStore')
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.StorageContext')
-    @patch('swarmzero.tools.retriever.pinecone_retrieve.VectorStoreIndex')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.PineconeVectorStore')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.StorageContext')
+    @patch('breachswarm.tools.retriever.pinecone_retrieve.VectorStoreIndex')
     def test_create_pod_index(self, VectorStoreIndexMock, StorageContextMock, PineconeVectorStoreMock):
         self.retriever._load_documents = MagicMock(return_value=(['doc1', 'doc2'], ['file1.txt', 'file2.txt']))
 
         index, file_names = self.retriever.create_pod_index(file_path='dummy_path')
 
         self.pinecone_client_mock.create_index.assert_called_once_with(
-            name="swarmzero-pinecone-pod", dimension=1536, metric="cosine", spec=ANY
+            name="breachswarm-pinecone-pod", dimension=1536, metric="cosine", spec=ANY
         )
-        self.pinecone_client_mock.Index.assert_called_once_with("swarmzero-pinecone-pod")
+        self.pinecone_client_mock.Index.assert_called_once_with("breachswarm-pinecone-pod")
         PineconeVectorStoreMock.assert_called_once()
         StorageContextMock.from_defaults.assert_called_once()
         VectorStoreIndexMock.from_documents.assert_called_once_with(
