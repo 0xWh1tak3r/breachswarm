8ae76b45e9691d4be9010beeb3b5a78c83c0efff
wh1tak3r <83946675+0xWh1tak3r@users.noreply.github.com>
2025-01-12 05:46:29 +0400

    Update nl_query.py

--- swarmzero/tools/agent_db/nl_query.py
+++ swarmzero/tools/agent_db/nl_query.py
@@ -5,7 +5,7 @@ from llama_index.core import SQLDatabase
 from llama_index.core.query_engine import NLSQLTableQueryEngine
 from llama_index.core.settings import Settings
 
-from swarmzero.sdk_context import SDKContext
+from breachswarm.sdk_context import SDKContext
 
 load_dotenv()
 
